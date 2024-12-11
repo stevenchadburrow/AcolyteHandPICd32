8b93d696f7f4cfc574037aec818a27a933ce5f51
Chad Burrow <stevenchadburrow@gmail.com>
2024-12-11 07:26:26 -0600

    Add files via upload

--- badapple.c
+++ badapple.c
@@ -9,11 +9,7 @@ void BadApple()
 		{
 			for (unsigned int x=0; x<SCREEN_X; x++)
 			{
-#ifdef SPLASH
-				screen_buffer[y*SCREEN_X+x] = splash_default[y * SCREEN_X + x];
-#else
 				screen_buffer[y*SCREEN_X+x] = 0x25; // blue-grey
-#endif
 			}
 		}
 
@@ -32,143 +28,115 @@ void BadApple()
 		}
 		else
 		{
-			// lock up
-			while (1)
-			{
-				for (unsigned int i=0; i<SCREEN_Y; i++)
-				{
-					for (unsigned int j=0; j<SCREEN_X; j++)
-					{
-						screen_buffer[i*SCREEN_X+j] = 0x00;
-					}
-				}
+			while (1) { } // lock up
+		} 
 
-				DelayMS(100);
-				DelayMS(100);
-				DelayMS(100);
-				DelayMS(100);
-
-				for (unsigned int i=0; i<SCREEN_Y; i++)
-				{
-					for (unsigned int j=0; j<SCREEN_X; j++)
-					{
-						screen_buffer[i*SCREEN_X+j] = 0xFF;
-					}
-				}
-
-				DelayMS(100);
-				DelayMS(100);
-				DelayMS(100);
-				DelayMS(100);
-			}
-		}  
-
-		unsigned int address_high = 0x0000, address_low = 0x0000;
+		T8CONbits.ON = 0; // turn off TMR8 
+		T9CONbits.ON = 0; // turn off TMR9
 
 		unsigned int x = 0x0000;
 		unsigned int y = 0x0000;
 		unsigned char value = 0x00;
 
+		unsigned int frames = 0;
+
 		unsigned char temp_value = 0x00;
 
-		unsigned int frames = 0;
+		unsigned long count = 0;
+
+		sdcard_disable();
+		sdcard_pump();
+		sdcard_longdelay(); // this is probably not needed
+		sdcard_enable();
+		sdcard_sendbyte(0x52); // CMD18 = 0x40 + 0x12 (18 in hex)
+		sdcard_sendbyte((0x0000&0x00FF));
+		sdcard_sendbyte(((0x0000&0xFF00) >> 8));
+		sdcard_sendbyte((0x0000&0x00FE)); // only blocks of 512 bytes
+		sdcard_sendbyte(0x00);
+		sdcard_sendbyte(0x01); // CRC (general)
+		temp_value = sdcard_waitresult(); // command response
+		if (temp_value == 0xFF) { return; }
+		else if (temp_value != 0x00) { return; } // expecting 0x00
+		temp_value = sdcard_waitresult(); // data packet starts with 0xFE
+		if (temp_value == 0xFF) { return; }
+		else if (temp_value != 0xFE) { return; }
+
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
+		sdcard_receivebyte(); // ???
 
 		while (frames < 13138) // total frames in the video
 		{
-			frames += 2; // only 30 FPS
+			frames += 1; // 60 FPS
 			
 			//x = 88;
 			//y = 104;
-			x = 128;
-			y = 180;
+			x = 136;
+			y = 96;
 
-			for (unsigned int l=0; l<12; l++)
+			for (unsigned int i=0; i<12; i++)
 			{
-				//sdcard_readblock(address_high, (unsigned int)(address_low + (j * 2)));
-
-				sdcard_disable();
-				sdcard_pump();
-				sdcard_longdelay(); // this is probably not needed
-				sdcard_enable();
-				sdcard_sendbyte(0x51); // CMD17 = 0x40 + 0x11 (17 in hex)
-				sdcard_sendbyte((unsigned char)((address_high)&0x00FF));
-				sdcard_sendbyte((unsigned char)(((address_low)&0xFF00) >> 8));
-				sdcard_sendbyte((unsigned char)((address_low)&0x00FE)); // only blocks of 512 bytes
-				sdcard_sendbyte(0x00);
-				sdcard_sendbyte(0x01); // CRC (general)
-				temp_value = sdcard_waitresult(); // command response
-				if (temp_value == 0xFF) { break; }
-				else if (temp_value != 0x00) { break; } // expecting 0x00
-				temp_value = sdcard_waitresult(); // data packet starts with 0xFE
-				if (temp_value == 0xFF) { break; }
-				else if (temp_value != 0xFE) { break; }
-
-				for (unsigned int i=0; i<16; i++)
+				if (frames != 1)
 				{
-					for (unsigned int j=0; j<32; j++) // packet of 512 bytes
-					{					
+					sdcard_receivebyte(); // data packet begins with 0xFE
+				}
+
+				for (unsigned int j=0; j<16; j++)
+				{	
+					for (unsigned int k=0; k<32; k++)
+					{
 						// get value from SDcard
 						value = sdcard_receivebyte();
 
-						for (unsigned int k=0; k<8; k++)
+						if (k == 15 || k == 31) // audio
 						{
-							if ((value & 0x01) == 0x01)
-							{
-								screen_buffer[y*SCREEN_X+x] = 0xFF;
-							}
-							else
-							{
-								screen_buffer[y*SCREEN_X+x] = 0x00;
-							}
-
-							value = (unsigned int)(value >> 1);
-
-							x += 1;
+							// Should add 0x07 before shifting in case of rounding???
+							PORTH = (((value >> 4) + 0x08) & 0x0F); 
 						}
+						else // comment the 'else' out to show the audio
+						{				
+							for (unsigned int l=0; l<8; l++)
+							{
+								if ((value & 0x01) == 0x01)
+								{
+									screen_buffer[y*SCREEN_X+x] = 0xFF;
+								}
+								else
+								{
+									screen_buffer[y*SCREEN_X+x] = 0x00;
+								}
+
+								value = (unsigned int)(value >> 1);
+
+								x += 1;
+							}
+						}
+
+						// fine tune delay here
+						count = (unsigned long)(63);
+						_CP0_SET_COUNT(0);
+						while (count > _CP0_GET_COUNT());
 					}
 
 					y += 1;
-					
+				
 					//x = 88;
-					x = 128;
+					x = 136;
 				}
 
-				temp_value = sdcard_receivebyte(); // data packet ends with 0x55 then 0xAA
-				temp_value = sdcard_receivebyte(); // ignore here
-				sdcard_disable();
+				sdcard_receivebyte();
+				sdcard_receivebyte(); // data packet ends with 0x55 then 0xAA
 
-				if (address_low == 0xFFFE)
-				{
-					address_low = 0;
-					
-					address_high += 1; 
-				}
-				else
-				{
-					address_low += 2;
-				}
-			}
-
-			// uncomment if you want to skip next frame entirely!
-			//for (unsigned int l=0; l<12; l++)
-			//{
-			//	if (address_low == 0xFFFE)
-			//	{
-			//		address_low = 0;
-			//		
-			//		address_high += 1; 
-			//	}
-			//	else
-			//	{
-			//		address_low += 2;
-			//	}
-			//}
-			
-			// fine tune delay here
-			unsigned long count = 0x00000000;
-			count = (unsigned long)(((unsigned long)((SYS_FREQ / 1000) * 17) / 5) / 2); // Convert microseconds us into how many clock ticks it will take
-			_CP0_SET_COUNT(0); // Set Core Timer count to 0
-			while (count > _CP0_GET_COUNT()); // Wait until Core Timer count reaches the number we calculated earlier
+				sdcard_receivebyte(); // ???
+			}			
 		}
 	}
 }
