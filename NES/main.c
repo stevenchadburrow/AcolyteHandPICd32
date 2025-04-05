ad2f4a92a9addb3da84efd6eda2ba2f611df2d22
Chad Burrow <stevenchadburrow@gmail.com>
2025-04-05 05:13:56 -0500

    Add files via upload

--- NES/main.c
+++ NES/main.c
@@ -41,6 +41,35 @@ Increment line number and change addresses on separate interrupt!
 */ 
 
 /*
+XGA Signal 1024 x 768 @ 60 Hz timing
+
+Interested in easy to use VGA solution for embedded applications? Click here!
+
+General timing
+Screen refresh rate	60 Hz
+Vertical refresh	48.363095238095 kHz
+Pixel freq.	65.0 MHz
+Horizontal timing (line)
+Polarity of horizontal sync pulse is negative.
+
+Scanline part	Pixels	Time [µs]
+Visible area	1024	15.753846153846
+Front porch	24	0.36923076923077
+Sync pulse	136	2.0923076923077
+Back porch	160	2.4615384615385
+Whole line	1344	20.676923076923
+Vertical timing (frame)
+Polarity of vertical sync pulse is negative.
+
+Frame part	Lines	Time [ms]
+Visible area	768	15.879876923077
+Front porch	3	0.062030769230769
+Sync pulse	6	0.12406153846154
+Back porch	29	0.59963076923077
+Whole frame	806	16.6656
+*/
+
+/*
 // Video Color Signals
 PORTH
 
